e9ad00d00ea5d40f3c96c2813c9ed9c8f6060aa6
ljhee92 <weilainib7@gmail.com>
2024-12-12 15:07:01 +0900

    feat: inputView Pattern 형식 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,17 @@
 package view;
 
+import util.ErrorMessage;
 import util.InputReader;
 import util.OutputWriter;
 
+import java.util.regex.Pattern;
+
 public class InputView implements InputReader, OutputWriter {
-    
+    private static final Pattern PATTERN = Pattern.compile("^([a-z]+)+$");
+
+    private void validate(String input) {
+        if (!PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_EXCEPTION.getMessage());
+        }
+    }
 }
